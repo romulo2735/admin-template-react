4e03074c239e230b2074eec62b1539d8a4af91d4
Romulo Sousa <romulosousa27@hotmail.com>
2020-04-16 20:45:45 -0300

    Página de recuperar senha

---
+++ src/components/EsqueceuSenha.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import {Container, Col, Row, Form, Card, CardBody, Input, InputGroup, Button} from 'reactstrap';
+import useClass from "../hooks/add-class";
+import {Link} from "react-router-dom";
+
+const EsqueceuSenha = () => {
+  useClass('bg-blue')
+  return (
+      <div className={'mt-5 flex-row align-items-center'}>
+        <Container>
+          <Row className={'justify-content-center'}>
+            <Col className={'col-6'}>
+              <Card>
+                <CardBody>
+                  <Form>
+                    <h1>Esqueceu a senha!</h1>
+                    <p>Digite o email para receber o link de redefinição.</p>
+
+                    <InputGroup className={'mb-3'}>
+                      <Input type="email" placeholder="E-mail"/>
+                    </InputGroup>
+
+                    <InputGroup className={'mb-3'}>
+                      <Button outline color={'primary'}>Enviar</Button>
+                      <Link to={'/login'}>
+                        <Button color={'link'}>Login</Button>
+                      </Link>
+                    </InputGroup>
+                  </Form>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+  );
+}
+
+export default EsqueceuSenha;
