c0588ec6525df81028bcdb495dc8e80ef176dca1
Romulo Sousa <romulosousa27@hotmail.com>
2020-04-16 20:46:22 -0300

    Modificação do componente de login, usando const para inserir hooks

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,38 +1,45 @@
 import React, {Component} from 'react';
 import {Container, Col, Row, Form, Card, CardBody, Input, InputGroup, Button} from 'reactstrap';
+import useClass from "../hooks/add-class";
+import {Link} from "react-router-dom";
 
-export default class Login extends Component {
-  render() {
-    return (
-        <div className={'mt-5 flex-row align-items-center'}>
-          <Container>
-            <Row className={'justify-content-center'}>
-              <Col className={'col-6'}>
-                <Card>
-                  <CardBody>
-                    <Form>
-                      <h1>Login</h1>
-                      <p>Entre com o email e a senha para acessar!</p>
+const Login = () => {
+  useClass('bg-blue')
+  return (
+      <div className={'mt-5 flex-row align-items-center'}>
+        <Container>
+          <Row className={'justify-content-center'}>
+            <Col className={'col-6'}>
+              <Card>
+                <CardBody>
+                  <Form>
+                    <h1>Login</h1>
+                    <p>Digite o email e senha para acessar! Sem cadastro? <Link to={'/novo-cadastro'} >Cadastrar-se</Link></p>
 
-                      <InputGroup className={'mb-3'}>
-                        <Input type="email" placeholder="E-mail"/>
-                      </InputGroup>
+                    <InputGroup className={'mb-3'}>
+                      <Input type="email" placeholder="E-mail"/>
+                    </InputGroup>
 
-                      <InputGroup className={'mb-3'}>
-                        <Input type="password" placeholder="Senha"/>
-                      </InputGroup>
+                    <InputGroup className={'mb-3'}>
+                      <Input type="password" placeholder="Senha"/>
+                    </InputGroup>
 
-                      <InputGroup className={'mb-3'}>
-                        <Button color={'primary'}>Acessar</Button>
-                      </InputGroup>
+                    <InputGroup className={'mb-3'}>
+                      <Button outline color={'primary'}>Acessar</Button>
+                    </InputGroup>
 
-                    </Form>
-                  </CardBody>
-                </Card>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-    );
-  }
+                    <InputGroup className={'mb-3'}>
+                      <Link to={'/esqueceu-a-senha'} >Esqueceu a senha?  </Link>
+                    </InputGroup>
+
+                  </Form>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+  );
 }
+
+export default Login;
