58fc09161857b2d8fbdd8d1b887157b18331d93a
Romulo Sousa <romulosousa27@hotmail.com>
2020-04-17 23:06:45 -0300

    Rotas dinamicas

--- src/containers/AppLayout.js
+++ src/containers/AppLayout.js
@@ -1,11 +1,23 @@
 import React, {Component} from 'react';
+import {Switch, Route, Link} from 'react-router-dom';
+import Routes from "./routes";
 
 export default class AppLayout extends Component {
-    render() {
-        return (
-            <>
-                <h1>Home Page</h1>
-            </>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Switch>
+          {
+            Routes.map((route, index) => (
+              <Route
+                  path={route.path}
+                  exact
+                  render={props => <route.template {...props}  />}
+              />
+            ))
+          }
+        </Switch>
+      </div>
+    );
+  }
 }
