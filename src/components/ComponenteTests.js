2e870a5e53c4775dfa4734c67161872f81b1d71f
Romulo Sousa <romulosousa27@hotmail.com>
2020-04-18 00:16:43 -0300

    Componente de teste

---
+++ src/components/ComponenteTests.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ComponenteTests extends Component {
+  render() {
+    return (
+        <div>
+          Componente Teste
+        </div>
+    );
+  }
+}
+
+export default ComponenteTests;
