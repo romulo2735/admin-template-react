152eb56bae58a2677538413ec2ebe95ecb8ad63a
Romulo Sousa <romulosousa27@hotmail.com>
2020-04-18 00:17:15 -0300

    Rotas

--- src/containers/routes.js
+++ src/containers/routes.js
@@ -1,10 +1,10 @@
-import Login from "../components/Login";
+import ComponenteTests from "../components/ComponenteTests";
 
 const Routes = () => ([
   {
     path: '',
     name: '',
-    template: Login
+    template: ComponenteTests
   }
 ]);
 
