1fbf8deb42b93c3bdb66af6bd0edae3895329c3e
Romulo Sousa <romulosousa27@hotmail.com>
2020-04-18 00:17:01 -0300

    Estilizando o componente de container

--- src/containers/AppLayout.js
+++ src/containers/AppLayout.js
@@ -1,23 +1,34 @@
 import React, {Component} from 'react';
-import {Switch, Route, Link} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
+
 import Routes from "./routes";
+import Header from "../components/admin/Header";
+import SideBar from "../components/admin/SideBar";
 
 export default class AppLayout extends Component {
   render() {
     return (
-      <div>
-        <Switch>
-          {
-            Routes.map((route, index) => (
-              <Route
-                  path={route.path}
-                  exact
-                  render={props => <route.template {...props}  />}
-              />
-            ))
-          }
-        </Switch>
-      </div>
+        <div className="d-flex" id="main-wrapper">
+          <SideBar/>
+
+          <div className="content-wrapper">
+            <Header/>
+            <Switch>
+              {
+                Routes.map((route, index) => (
+                    <div className="content-dynamic">
+                      <Route
+                          key={index}
+                          path={route.path}
+                          exact
+                          render={props => <route.template {...props}  />}
+                      />
+                    </div>
+                ))
+              }
+            </Switch>
+          </div>
+        </div>
     );
   }
 }
