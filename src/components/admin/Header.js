5b65c1488ab1324275ad4d0159d415bc41d95ec6
Romulo Sousa <romulosousa27@hotmail.com>
2020-04-18 00:16:11 -0300

    Header

--- src/components/admin/Header.js
+++ src/components/admin/Header.js
@@ -1,14 +1,16 @@
 import React from "react";
-import {Navbar, NavbarBrand, NavbarToggler} from "reactstrap";
+import {Navbar, NavbarBrand, NavbarToggler, Nav} from "reactstrap";
 
 export default function Header() {
   return (
       <header>
-        <Navbar color="light">
+        <Navbar color="light" light>
           <NavbarBrand href="/">
             <img src="https://via.placeholder.com/150x50" alt=""/>
           </NavbarBrand>
-          <NavbarToggler />
+          <Nav className="mr-auto" navbar>
+            <NavbarToggler/>
+          </Nav>
         </Navbar>
       </header>
   );
