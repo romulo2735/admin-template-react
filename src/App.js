cefde3622d2eba142337f6992de2eb3aedb098c8
Romulo Sousa <romulosousa27@hotmail.com>
2020-04-27 21:47:59 -0300

    Corrigindo o namespace dos componentes de paginas/

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import './styles/app.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 import AppLayout from "./containers/AppLayout";
-import Login from "./components/Login";
+import Login from "./pages/Login";
 import NotFound from "./components/Errors/NotFound";
-import EsqueceuSenha from "./components/EsqueceuSenha";
-import Registrar from "./components/Registrar";
+import EsqueceuSenha from "./pages/EsqueceuSenha";
+import Registrar from "./pages/Registrar";
 
 export default function App() {
   return (
       <Router>
         <Switch>
-          <Route exact path="/" name={'Home'} render={props => <AppLayout {...props} />} />
           <Route exact path="/login" name={'Login'} render={props => <Login {...props} />}/>
           <Route exact path="/esqueceu-a-senha" name={'Redefinir Senha'} render={props => <EsqueceuSenha {...props} />}/>
           <Route exact path="/novo-cadastro" name={'Novo Cadastro'} render={props => <Registrar {...props} />}/>
+          <Route exact path="/" name={'Home'} render={props => <AppLayout {...props} />} />
 
           <Route exact path="*" component={NotFound}/>
         </Switch>
