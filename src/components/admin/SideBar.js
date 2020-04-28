38ffdb20c4daa7e9204fa1ffa1850f5c5ddcee3c
Romulo Sousa <romulosousa27@hotmail.com>
2020-04-27 21:48:47 -0300

    Carregando os componentes dinamicamente no sidebart.

--- src/components/admin/SideBar.js
+++ src/components/admin/SideBar.js
@@ -1,12 +1,27 @@
-import React from "react";
-import {Navbar, NavbarBrand, NavbarToggler} from "reactstrap";
+import React, {useEffect} from "react";
+import Templates from './../../containers/routes';
 
 export default function SideBar(props) {
+  useEffect(() => {
+    document.getElementById('main-wrapper').classList.toggle('toggled');
+    return () => {
+      document.getElementById('main-wrapper').classList.remove('toggled');
+    }
+  }, []);
+
   return (
       <aside id="sidebar">
-        <div className="sidebar-title">React Admin</div>
+        <div className="sidebar-title">
+          <h2>React Admin</h2>
+        </div>
         <div className="list-group">
-          <a className="list-group-item" href="">Link</a>
+          <a className="list-group-item" href="/">Home</a>
+          {
+            Templates.map((template, index) => (
+                <a className="list-group-item" href={template.path} key={index}>{template.name}</a>
+            ))
+          }
+
         </div>
       </aside>
   );
