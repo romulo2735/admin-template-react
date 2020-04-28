97f32edaad8d565a3abac2cc82b06b966615fa1d
Romulo Sousa <romulosousa27@hotmail.com>
2020-04-27 21:49:14 -0300

    Button de esconder e mostrar o sidebar do dashboard

--- src/components/admin/Header.js
+++ src/components/admin/Header.js
@@ -2,14 +2,19 @@ import React from "react";
 import {Navbar, NavbarBrand, NavbarToggler, Nav} from "reactstrap";
 
 export default function Header() {
+  function handleClickToSidebar(e) {
+    e.preventDefault();
+    document.getElementById('main-wrapper').classList.toggle('toggled');
+  }
+
   return (
       <header>
         <Navbar color="light" light>
           <NavbarBrand href="/">
             <img src="https://via.placeholder.com/150x50" alt=""/>
           </NavbarBrand>
-          <Nav className="" navbar>
-            <NavbarToggler/>
+          <Nav className="mr-auto" navbar>
+            <NavbarToggler onClick={handleClickToSidebar}/>
           </Nav>
         </Navbar>
       </header>
