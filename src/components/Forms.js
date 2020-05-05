eee891401f7d07f921b2e09fd09a57dc3bff6b07
Romulo Sousa <romulosousa27@hotmail.com>
2020-05-04 22:18:50 -0300

    adicionando validação nos formulários

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,34 +1,52 @@
-import React from 'react';
-import {FormGroup, Label, Input, Row, Col, Card, CardHeader, CardBody, CardFooter, Button} from 'reactstrap';
+import React from "react";
+import {
+  FormGroup,
+  Label,
+  Input,
+  Row,
+  Col,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Button,
+} from "reactstrap";
+import {useForm} from "react-hook-form";
 
 export default function Forms() {
+  const {handleSubmit, watch, errors, register} = useForm();
+
+  function onSubmit(data) {
+    console.log(data);
+  }
 
   return (
       <div>
         <Row>
           <Col xs={12} sm={12} md={12}>
             <Card>
-              <CardHeader>
-                Forms
-              </CardHeader>
+              <CardHeader>Forms</CardHeader>
               <CardBody>
-                <form onSubmit>
+                <form onSubmit={onSubmit}>
                   <FormGroup>
                     <Label for="name">Name:</Label>
-                    <Input type="text" placeholder="Insert your name"/>
+                    <Input
+                        type="text"
+                        name="name"
+                        placeholder="Insert your name"
+                        innerRef={register({required: true})}
+                    />
+                    {errors && <span className="text-danger">field is required</span>}
                   </FormGroup>
                   <FormGroup>
-                    <Button>Send</Button>
+                    <Button type="submit">Send</Button>
                   </FormGroup>
                 </form>
               </CardBody>
-              <CardFooter>
-                Footer
-              </CardFooter>
+              <CardFooter>Footer</CardFooter>
             </Card>
           </Col>
         </Row>
       </div>
   );
 }
-
