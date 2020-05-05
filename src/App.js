648e06e2e8e78b1b34f68dd5bb50615db6017136
Romulo Sousa <romulosousa27@hotmail.com>
2020-05-04 22:35:24 -0300

    adicionando provider de notificações

--- src/App.js
+++ src/App.js
@@ -9,19 +9,21 @@ import EsqueceuSenha from "./pages/EsqueceuSenha";
 import Registrar from "./pages/Registrar";
 import Cards from './components/Cards';
 import Forms from './components/Forms';
+import {ToastProvider} from 'react-toast-notifications';
 
 export default function App() {
   return (
       <Router>
         <Switch>
-          <Route exact path="/login" name={'Login'} render={props => <Login {...props} />}/>
-          <Route exact path="/esqueceu-a-senha" name={'Redefinir Senha'} render={props => <EsqueceuSenha {...props} />}/>
-          <Route exact path="/novo-cadastro" name={'Novo Cadastro'} render={props => <Registrar {...props} />}/>
-          <Route exact path="/cards" name={'Cards'} render={props => <Cards {...props} />} />
-          <Route exact path="/forms" name={'Forms'} render={props => <Forms {...props} />} />
-          <Route exact path="/" name={'Home'} render={props => <AppLayout {...props} />} />
-
-          <Route exact path="*" component={NotFound}/>
+          <ToastProvider>
+            <Route exact path="/login" name="Login" render={props => <Login {...props} />}/>
+            <Route exact path="/esqueceu-a-senha" name="Redefinir Senha" render={props => <EsqueceuSenha {...props} />}/>
+            <Route exact path="/novo-cadastro" name="Novo Cadastro" render={props => <Registrar {...props} />}/>
+            <Route exact path="/cards" name="Cards" render={props => <Cards {...props} />} />
+            <Route exact path="/forms" name="Forms" render={props => <Forms {...props} />} />
+            <Route exact path="/" name="Home" render={props => <AppLayout {...props} />} />
+            <Route exact path="*" component={NotFound}/>
+          </ToastProvider>
         </Switch>
       </Router>
   );
