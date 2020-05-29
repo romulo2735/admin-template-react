3334f392660c0610267a9a349b65829fd909757d
Romulo Sousa <romulosousa27@hotmail.com>
2020-05-29 19:38:19 -0300

    corrigindo o erro de path para acessar as rotas dos componentes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './styles/app.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Redirect, Switch, Route} from 'react-router-dom'
+
+import {ToastProvider} from 'react-toast-notifications';
+import { ToastCustom } from './components/ToastCustom';
 
 import AppLayout from "./containers/AppLayout";
 import Login from "./pages/Login";
 import NotFound from "./components/Errors/NotFound";
 import EsqueceuSenha from "./pages/EsqueceuSenha";
 import Registrar from "./pages/Registrar";
-import Cards from './components/Cards';
-import Forms from './components/Forms';
-import {ToastProvider} from 'react-toast-notifications';
-import { ToastCustom } from './components/ToastCustom';
 
 export default function App() {
   return (
@@ -20,10 +19,8 @@ export default function App() {
             <Route exact path="/login" name="Login" render={props => <Login {...props} />}/>
             <Route exact path="/esqueceu-a-senha" name="Redefinir Senha" render={props => <EsqueceuSenha {...props} />}/>
             <Route exact path="/novo-cadastro" name="Novo Cadastro" render={props => <Registrar {...props} />}/>
-            <Route exact path="/cards" name="Cards" render={props => <Cards {...props} />} />
-            <Route exact path="/forms" name="Forms" render={props => <Forms {...props} />} />
-            <Route exact path="/" name="Home" render={props => <AppLayout {...props} />} />
-            <Route exact path="*" component={NotFound}/>
+            <Route exact path="/page-not-found" name="NotFound" component={NotFound}/>
+            <Route path="/" name="Home" render={props => <AppLayout {...props} />} />
           </ToastProvider>
         </Switch>
       </Router>
