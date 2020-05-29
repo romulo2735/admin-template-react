e7e4b95e38c83ffe8f92c4148aff1cbd9709756a
Romulo Sousa <romulosousa27@hotmail.com>
2020-05-29 19:38:50 -0300

    removendo o link default de barra para inserir o dash default

--- src/components/admin/SideBar.js
+++ src/components/admin/SideBar.js
@@ -1,29 +1,26 @@
-import React, {useEffect} from "react";
-import Templates from './../../containers/routes';
+import React, { useEffect } from "react";
+import Templates from "./../../containers/routes";
 
 export default function SideBar(props) {
   useEffect(() => {
-    document.getElementById('main-wrapper').classList.toggle('toggled');
+    document.getElementById("main-wrapper").classList.toggle("toggled");
     return () => {
-      document.getElementById('main-wrapper').classList.remove('toggled');
-    }
+      document.getElementById("main-wrapper").classList.remove("toggled");
+    };
   }, []);
 
   return (
-      <aside id="sidebar">
-        <div className="sidebar-title">
-          <h2>React Admin</h2>
-        </div>
-        <div className="list-group">
-          <a className="list-group-item" href="/">Home</a>
-          {
-            Templates.map((template, index) => (
-                <a className="list-group-item" href={template.path} key={index}>{template.name}</a>
-            ))
-          }
-
-        </div>
-      </aside>
+    <aside id="sidebar">
+      <div className="sidebar-title">
+        <h2>React Admin</h2>
+      </div>
+      <div className="list-group">
+        {Templates.map((template, index) => (
+          <a className="list-group-item" href={template.path} key={index}>
+            {template.name}
+          </a>
+        ))}
+      </div>
+    </aside>
   );
 }
-
